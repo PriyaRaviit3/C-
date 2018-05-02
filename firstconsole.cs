f9ab1e5020358a00c29fe506bbef8d90c40cc005
Priya Ravi <34087852+PriyaRaviit3@users.noreply.github.com>
2018-05-02 12:30:40 +0530

    Add files via upload

---
+++ firstconsole.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace firstconsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("hi migen");
+            Console.WriteLine("hi dhanya");
+            Console.ReadLine();
+        }
+    }
+}
